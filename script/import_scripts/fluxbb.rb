90ec3ad7d7e9e00bf2268e63405c4a263630cb77
lilydjwg <lilydjwg@gmail.com>
2025-08-03 18:46:28 +0800

    import_users/fluxbb: skip importing banned users

--- script/import_scripts/fluxbb.rb
+++ script/import_scripts/fluxbb.rb
@@ -42,7 +42,7 @@ class ImportScripts::FluxBB < ImportScripts::Base
     import_users
     import_categories
     import_posts
-    suspend_users
+    # suspend_users
   end
 
   def import_groups
@@ -64,7 +64,8 @@ class ImportScripts::FluxBB < ImportScripts::Base
   def import_users
     puts "", "creating users"
 
-    total_count = mysql_query("SELECT count(*) count FROM #{FLUXBB_PREFIX}users WHERE email like '%@%' and (num_posts != 0 or registered > 1722661304);").first["count"]
+    total_count = mysql_query("SELECT count(*) count FROM #{FLUXBB_PREFIX}users WHERE email like '%@%' AND (num_posts != 0 OR registered > 1722661304);").first["count"]
+    banned_users = mysql_query("SELECT username FROM #{FLUXBB_PREFIX}bans;").map { |row| row["username"] }.to_set
 
     batches(BATCH_SIZE) do |offset|
       results =
@@ -73,7 +74,7 @@ class ImportScripts::FluxBB < ImportScripts::Base
                 registration_ip registration_ip_address, last_visit last_visit_time,
                 last_email_sent last_emailed_at, location, group_id, signature bio
          FROM #{FLUXBB_PREFIX}users
-         WHERE email like '%@%' AND num_posts != 0 and (num_posts != 0 or registered > 1722661304)
+         WHERE email like '%@%' AND (num_posts != 0 OR registered > 1722661304)
          LIMIT #{BATCH_SIZE}
          OFFSET #{offset};",
         )
@@ -83,6 +84,9 @@ class ImportScripts::FluxBB < ImportScripts::Base
       next if all_records_exist? :users, results.map { |u| u["id"].to_i }
 
       create_users(results, total: total_count, offset: offset) do |user|
+
+        next if banned_users.include? user["username"]
+
         {
           id: user["id"],
           email: user["email"],
